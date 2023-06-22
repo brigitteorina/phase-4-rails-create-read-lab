274d859eee5a7009e7b5a24fd1c8803c658ca646
brigitteorina <brigitteorina@gmail.com>
2023-06-22 13:18:58 +0300

    completed assignment

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  #This code will define the routes for the index, show, and create actions of the PlantsController
+  resources :plants, only: [:index, :show,:create]
 end
